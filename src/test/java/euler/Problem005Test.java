65b12fb738419059fe1a160e02f2ccce3076ecb0
Otavio Macedo <otaviomacedo@gmail.com>
2011-06-20 16:55:29 -0300

    Minor refactoring.

--- src/test/java/euler/Problem005Test.java
+++ src/test/java/euler/Problem005Test.java
@@ -12,17 +12,17 @@ public class Problem005Test {
 
     @Test
     public void test1() throws Exception {
-        assertThat(LeastCommonMultiple.between(range(10)), is(2520));
+        assertThat(LeastCommonMultiple.between(range(1, 10)), is(2520));
     }
 
     @Test
     public void test2() throws Exception {
-        assertThat(LeastCommonMultiple.between(range(20)), is(232792560));
+        assertThat(LeastCommonMultiple.between(range(1, 20)), is(232792560));
     }
 
-    private static List<Integer> range(int max) {
+    private static List<Integer> range(int min, int max) {
         List<Integer> range = newArrayList();
-        for (int i = 1; i <= max; i++) {
+        for (int i = min; i <= max; i++) {
             range.add(i);
         }
         return range;
